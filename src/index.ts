e9481590b2f4330c217dcddd91887a82fd0bbc56
Aurélien Grimaud <a.grimaud@involt.io>
2025-06-03 15:17:39 +0200

    fix: update Easing type to EasingConfiguration for consistency

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export type {
   SourceColorConfiguration,
   ExchangeConfiguration,
   ExtractOfBaseConfiguration,
-  Easing,
+  EasingConfiguration,
   PresetConfiguration,
   ScaleConfiguration,
   LockedSourceColorsConfiguration,
