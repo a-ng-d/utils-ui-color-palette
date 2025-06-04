00a532f464ef41731792e587fce57086cade4848
Aurélien Grimaud <a.grimaud@involt.io>
2025-06-05 01:13:58 +0200

    chore: add import/order rule to ESLint configuration for improved module organization

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,4 +8,44 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  plugins: ['import'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern: '@a_ng_d/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@tps/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@modules/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        'newlines-between': 'never',
+        alphabetize: {
+          order: 'desc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 }
