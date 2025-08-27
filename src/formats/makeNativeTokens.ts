45627981cb3ee005a3d9e2e4964305edd743c75a
Aurélien Grimaud <a.grimaud@involt.io>
2025-08-27 23:15:39 +0200

    feat: improve the theme management of the native tokens

--- src/formats/makeNativeTokens.ts
+++ src/formats/makeNativeTokens.ts
@@ -4,26 +4,12 @@ import {
   PaletteDataColorItem,
   PaletteDataShadeItem,
 } from '@tps/data.types'
+import { Case } from '@a_ng_d/figmug-utils'
 
 const makeNativeTokens = (paletteData: PaletteData) => {
-  const workingThemes =
-      paletteData.themes.filter((theme) => theme.type === 'custom theme')
-        .length === 0
-        ? paletteData.themes.filter((theme) => theme.type === 'default theme')
-        : paletteData.themes.filter((theme) => theme.type === 'custom theme'),
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    json: { [key: string]: any } = {
-      $themes: [],
-      $metadata: {
-        activeThemes: [],
-        tokenSetOrder: [],
-        activeSets: [],
-      },
-    }
-
   const paletteName = paletteData.name
 
-  const model = (
+  const colorModel = (
     color: PaletteDataColorItem,
     shade: PaletteDataShadeItem,
     source: PaletteDataShadeItem
@@ -42,39 +28,70 @@ const makeNativeTokens = (paletteData: PaletteData) => {
     }
   }
 
+  const themeModel = (name: string, description: string) => {
+    return {
+      name: name,
+      group: 'Modes',
+      description: description,
+      selectedTokenSets: {
+        [`${paletteName}/${name}`]: 'enabled',
+      },
+    }
+  }
+
+  const workingThemes =
+      paletteData.themes.filter((theme) => theme.type === 'custom theme')
+        .length === 0
+        ? paletteData.themes.filter((theme) => theme.type === 'default theme')
+        : paletteData.themes.filter((theme) => theme.type === 'custom theme'),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    json: { [key: string]: any } = {
+      $themes: workingThemes
+        .filter((theme) => theme.type === 'custom theme')
+        .map((theme) => themeModel(theme.name, theme.description)),
+      $metadata: {
+        activeThemes: [],
+        tokenSetOrder: workingThemes
+          .filter((theme) => theme.type === 'custom theme')
+          .map(
+            (theme) =>
+              `${new Case(paletteName).doSnakeCase()}/${new Case(theme.name).doSnakeCase()}`
+          ),
+        activeSets: [],
+      },
+    }
+
   if (workingThemes[0].type === 'custom theme')
     workingThemes.forEach((theme) => {
+      json[`${paletteName}/${theme.name}`] = {}
+
       theme.colors.forEach((color) => {
         const source = color.shades.find(
           (shade) => shade.type === 'source color'
         )
 
-        json[`${theme.name}/${color.name}`] = {}
         color.shades.forEach((shade) => {
           if (shade && source)
-            json[`${theme.name}/${color.name}`][shade.name] = model(
-              color,
-              shade,
-              source
-            )
+            json[`${paletteName}/${theme.name}`][
+              `${new Case(color.name).doSnakeCase()}.${shade.name}`
+            ] = colorModel(color, shade, source)
         })
       })
     })
   else
     workingThemes.forEach((theme) => {
+      json[paletteName] = {}
+
       theme.colors.forEach((color) => {
         const source = color.shades.find(
           (shade) => shade.type === 'source color'
         )
 
-        json[`${paletteName}/${color.name}`] = {}
         color.shades.forEach((shade) => {
           if (shade && source)
-            json[`${paletteName}/${color.name}`][shade.name] = model(
-              color,
-              shade,
-              source
-            )
+            json[paletteName][
+              `${new Case(color.name).doSnakeCase()}.${shade.name}`
+            ] = colorModel(color, shade, source)
         })
       })
     })
